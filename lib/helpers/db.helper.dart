a927ea7c1abef97df89f1706404d0b5b70c15872
Jhonathan Costa <jhonathanstanley@gmail.com>
2021-02-05 00:36:12 -0300

    Adding DB helper class, Conta model and helper class.

---
+++ lib/helpers/db.helper.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DBHelper {
+  static Database _db;
+  static const String DB_NAME = 'jotarbsfinance.db';
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+    return _db;
+  }
+
+  initDb() async {
+    io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, DB_NAME);
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  _onCreate(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE Conta (id INTEGER PRIMARY KEY, descricao TEXT, saldo REAL)");
+  }
+}
