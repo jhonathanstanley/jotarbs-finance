60071d0f379cdadcd4a178f44091c7cec8d8afde
jhonathan.costa <jhonathan.costa@lgcns.com>
2021-02-08 11:32:50 -0300

    Fixing contas.page.dart

--- lib/screens/contas.page.dart
+++ lib/screens/contas.page.dart
@@ -3,36 +3,53 @@ import 'package:jotarbs_finance/helpers/conta.helper.dart';
 import 'package:jotarbs_finance/model/conta.dart';
 
 class Contas extends StatefulWidget {
+  var items = new List<Conta>();
+
   @override
   _ContasState createState() => _ContasState();
 }
 
 class _ContasState extends State<Contas> {
-  var contaHelper = ContaHelper();
-  
-  
+  var helper = new ContaHelper();
+  _ContasState() {
+    load();
+  }
+
+  Future load() async {
+    List<Conta> result = await helper.getAll();
+    if (result != null) {
+      return setState(() {
+        widget.items = result;
+      });
+    } else {
+      return null;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    load();
   }
-
- 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List>(
-        future: contaHelper.getAll(),
-        initialData: List(),
-        builder: (context, snapshot){
-          return snapshot.hasData ? 
-          ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (_, int position){
-              final item = snapshot.data[position];
-              
-            },
-              
+      body: Container(
+        child: ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = widget.items[index];
+            final descricao = item.descricao;
+            final saldo = item.saldo;
+            return Container(
+              child: ListTile(
+                title: Text(descricao),
+                subtitle: Text(saldo.toString()),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
